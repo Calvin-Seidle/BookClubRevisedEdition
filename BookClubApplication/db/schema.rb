be9b7df13ecd1c488bcc663a5f3e404789dd8609
Calvin-Seidle <seidlecalvin@gmail.com>
2017-05-02 16:33:32 +0200

    Added 5-star rating system and reviews

--- BookClubApplication/db/schema.rb
+++ BookClubApplication/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428130911) do
+ActiveRecord::Schema.define(version: 20170502134515) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20170428130911) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "book_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,6 +54,12 @@ ActiveRecord::Schema.define(version: 20170428130911) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.text     "about"
+    t.date     "birthday"
+    t.string   "id_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
